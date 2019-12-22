487337b25dec25c9db269b45e6d6ca94da0df97b
Kevin Jennison <kevin.jennison1@gmail.com>
2019-12-22 11:14:04 -0600

    Add auth page

---
+++ pages/auth.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <div>
+      <p>Sign in</p>
+      <div>
+        <p>todo</p>
+      </div>
+    </div>
+  )
+}
+
+Auth.propTypes = {}
+
+export default Auth
