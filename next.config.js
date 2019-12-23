f96e6cb76232ace8f04eb4f811b3ceb07bb99d96
Kevin Jennison <kevin.jennison1@gmail.com>
2019-12-23 12:53:47 -0600

    Support multiple dotenv files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
-require('dotenv').config()
+const fs = require('fs')
+
+const { NODE_ENV } = process.env
+if (!NODE_ENV) {
+  throw new Error(
+    'The NODE_ENV environment variable is required but was not specified.'
+  )
+}
+
+// Set env vars from appropiate `.env` files. See:
+// https://github.com/bkeepers/dotenv#what-other-env-files-can-i-use
+const dotEnvPath = './.env'
+const dotEnvFiles = [
+  `${dotEnvPath}.${NODE_ENV}.local`,
+  `${dotEnvPath}.${NODE_ENV}`,
+  // Don't include `.env.local` for the test environment.
+  NODE_ENV !== 'test' && `${dotEnvPath}.local`,
+  dotEnvPath,
+].filter(Boolean)
+
+dotEnvFiles.forEach(dotenvFile => {
+  if (fs.existsSync(dotenvFile)) {
+    // eslint-disable-next-line global-require
+    require('dotenv').config({
+      path: dotenvFile,
+    })
+  }
+})
 
 module.exports = {
   env: {
