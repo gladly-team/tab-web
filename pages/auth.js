e707778b97cf5e84e02a29dbb534f2ae1a472ad5
Kevin Jennison <kevin.jennison1@gmail.com>
2019-12-27 16:50:59 -0600

    Remove resolved FIXME

--- pages/auth.js
+++ pages/auth.js
@@ -2,8 +2,6 @@ import React from 'react'
 import FirebaseAuth from '../components/FirebaseAuth'
 
 const Auth = () => {
-  // FIXME: do not SSR FirebaseAuth.
-  // https://github.com/firebase/firebaseui-web/issues/213
   return (
     <div>
       <p>Sign in</p>
