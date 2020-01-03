1e1d0776853004f27ac26956a80acc6d60b2cdf8
Kevin Jennison <kevin.jennison1@gmail.com>
2020-01-03 11:37:47 -0500

    Define env vars in now.json

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ require('./env.js')
 module.exports = {
   // https://nextjs.org/docs#build-time-configuration
   env: {
-    FIREBASE_PUBLIC_API_KEY: process.env.FIREBASE_PUBLIC_API_KEY,
     FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
     FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL,
     FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-    GRAPHQL_SCHEMA_LOCATION: process.env.RELAY_ENDPOINT,
+    FIREBASE_PUBLIC_API_KEY: process.env.FIREBASE_PUBLIC_API_KEY,
+    GRAPHQL_SCHEMA_LOCATION: process.env.GRAPHQL_SCHEMA_LOCATION,
     RELAY_ENDPOINT: process.env.RELAY_ENDPOINT,
   },
 }
