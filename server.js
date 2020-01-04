15cf880122dec74ae090dce27c37205bbfbe67e7
Kevin Jennison <kevin.jennison1@gmail.com>
2020-01-04 13:01:23 -0500

    Break out cookie session middleware

--- server.js
+++ server.js
@@ -8,48 +8,21 @@ require('./env')
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const cookieSession = require('cookie-session')
 const nextJs = require('next')
 
 const { verifyIdToken } = require('./utils/auth/firebaseAdmin')
+const cookieSession = require('./utils/middleware/cookieSession')
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = nextJs({ dev })
 const handle = app.getRequestHandler()
 
-// Ensure that session secrets are set.
-if (
-  !(process.env.SESSION_SECRET_CURRENT && process.env.SESSION_SECRET_PREVIOUS)
-) {
-  throw new Error(
-    'Session secrets must be set as env vars `SESSION_SECRET_CURRENT` and `SESSION_SECRET_PREVIOUS`.'
-  )
-}
-
-// An array is useful for rotating secrets without invalidating old sessions.
-// The first will be used to sign cookies, and the rest to validate them.
-// https://github.com/expressjs/cookie-session#keys
-const sessionSecrets = [
-  process.env.SESSION_SECRET_CURRENT,
-  process.env.SESSION_SECRET_PREVIOUS,
-]
-
 app.prepare().then(() => {
   const server = express()
 
   server.use(bodyParser.json())
-  server.use(
-    cookieSession({
-      name: 'tabWebSession',
-      keys: sessionSecrets,
-      // TODO: set other options, such as "secure", "sameSite", etc.
-      // https://github.com/expressjs/cookie-session#cookie-options
-      maxAge: 604800000, // week
-      httpOnly: true,
-      overwrite: true,
-    })
-  )
+  server.use(cookieSession)
 
   // Update a value in the cookie so that the set-cookie will be sent.
   // Only changes every minute so that it's not sent with every request.
---
+++ utils/middleware/cookieSession.js
@@ -0,0 +1,28 @@
+const cookieSession = require('cookie-session')
+
+// Ensure that session secrets are set.
+if (
+  !(process.env.SESSION_SECRET_CURRENT && process.env.SESSION_SECRET_PREVIOUS)
+) {
+  throw new Error(
+    'Session secrets must be set as env vars `SESSION_SECRET_CURRENT` and `SESSION_SECRET_PREVIOUS`.'
+  )
+}
+
+// An array is useful for rotating secrets without invalidating old sessions.
+// The first will be used to sign cookies, and the rest to validate them.
+// https://github.com/expressjs/cookie-session#keys
+const sessionSecrets = [
+  process.env.SESSION_SECRET_CURRENT,
+  process.env.SESSION_SECRET_PREVIOUS,
+]
+
+module.exports = cookieSession({
+  name: 'tabWebSession',
+  keys: sessionSecrets,
+  // TODO: set other options, such as "secure", "sameSite", etc.
+  // https://github.com/expressjs/cookie-session#cookie-options
+  maxAge: 604800000, // week
+  httpOnly: true,
+  overwrite: true,
+})
