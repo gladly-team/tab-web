5e074385663d5edf40e02211194994a70abd1c63
Kevin Jennison <kevin.jennison1@gmail.com>
2020-01-05 12:36:36 -0500

    Export helper function to add the session to requests

--- utils/middleware/cookieSession.js
+++ utils/middleware/cookieSession.js
@@ -1,24 +1,26 @@
 import cookieSession from 'cookie-session'
 
-// Ensure that session secrets are set.
-if (
-  !(process.env.SESSION_SECRET_CURRENT && process.env.SESSION_SECRET_PREVIOUS)
-) {
-  throw new Error(
-    'Session secrets must be set as env vars `SESSION_SECRET_CURRENT` and `SESSION_SECRET_PREVIOUS`.'
-  )
-}
+export const addSession = (req, res) => {
+  // Ensure that session secrets are set.
+  if (
+    !(process.env.SESSION_SECRET_CURRENT && process.env.SESSION_SECRET_PREVIOUS)
+  ) {
+    throw new Error(
+      'Session secrets must be set as env vars `SESSION_SECRET_CURRENT` and `SESSION_SECRET_PREVIOUS`.'
+    )
+  }
 
-// An array is useful for rotating secrets without invalidating old sessions.
-// The first will be used to sign cookies, and the rest to validate them.
-// https://github.com/expressjs/cookie-session#keys
-const sessionSecrets = [
-  process.env.SESSION_SECRET_CURRENT,
-  process.env.SESSION_SECRET_PREVIOUS,
-]
+  // An array is useful for rotating secrets without invalidating old sessions.
+  // The first will be used to sign cookies, and the rest to validate them.
+  // https://github.com/expressjs/cookie-session#keys
+  const sessionSecrets = [
+    process.env.SESSION_SECRET_CURRENT,
+    process.env.SESSION_SECRET_PREVIOUS,
+  ]
 
-export default handler => (req, res) => {
-  const addSession = cookieSession({
+  // Example:
+  // https://github.com/billymoon/micro-cookie-session
+  const includeSession = cookieSession({
     name: 'tabWebSession',
     keys: sessionSecrets,
     // TODO: set other options, such as "secure", "sameSite", etc.
@@ -27,9 +29,14 @@ export default handler => (req, res) => {
     httpOnly: true,
     overwrite: true,
   })
+  includeSession(req, res, () => {})
+}
 
-  // Example:
-  // https://github.com/billymoon/micro-cookie-session
-  addSession(req, res, () => {})
+export default handler => (req, res) => {
+  try {
+    addSession(req, res)
+  } catch (e) {
+    return res.status(500).json({ error: 'Could not get user session.' })
+  }
   return handler(req, res)
 }
