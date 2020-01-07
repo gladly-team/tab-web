3bb3dddcdd7a61f7a9753dde9e1ff403dad2d8fc
Kevin Jennison <kevin.jennison1@gmail.com>
2020-01-07 12:31:52 -0500

    Add auth info to HTML in JSON

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,29 @@
+/* eslint react/no-danger: 0 */
 import React from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
+import { get } from 'lodash/object'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class CustomDocument extends Document {
   render() {
+    // https://github.com/zeit/next.js/issues/3043#issuecomment-334521241
+    // https://github.com/zeit/next.js/issues/2252#issuecomment-353992669
+    const { authUserFromSession, authUserToken } = this.props
+    const authInfo = {
+      authUserFromSession,
+      authUserToken,
+    }
     return (
       <Html>
-        <Head />
+        <Head>
+          <script
+            id="__TAB_WEB_AUTH_INFO"
+            type="application/json"
+            dangerouslySetInnerHTML={{
+              __html: JSON.stringify(authInfo, null, 2),
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
@@ -17,10 +34,21 @@ class CustomDocument extends Document {
 }
 
 CustomDocument.getInitialProps = async ctx => {
+  // Get the AuthUser and user token. This is set in _app.js.
+  const authUserFromSession = get(ctx, 'tabCustomData.authUser', null)
+  const authUserToken = get(ctx, 'tabCustomData.authUserToken', null)
+
   const initialProps = await Document.getInitialProps(ctx)
-  return { ...initialProps }
+  return { ...initialProps, authUserFromSession, authUserToken }
 }
 
-CustomDocument.propTypes = {}
+CustomDocument.propTypes = {
+  authUserFromSession: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    emailVerified: PropTypes.bool.isRequired,
+  }),
+  authUserToken: PropTypes.string,
+}
 
 export default CustomDocument
