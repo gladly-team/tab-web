5aaaad092a621355d236f4222caefee31cafcd2b
Kevin Jennison <kevin.jennison1@gmail.com>
2020-01-28 16:01:56 -0800

    Change the service worker prefix

--- next.config.js
+++ next.config.js
@@ -28,10 +28,13 @@ const nextConfig = {
   },
   // Modify our service worker manifest.
   transformManifest: manifest => ['/'].concat(manifest), // add the homepage to the cache
-  // Whether to enable the SW in development. Note this may not work
+  // Whether to enable the service worker in development. Note this may not work
   // if we don't have a custom _error.js file:
   // https://github.com/hanford/next-offline/issues/190#issuecomment-535278921
   generateInDevMode: false,
+  // The base path from which to serve the service worker. This affects the scope.
+  // https://developers.google.com/web/ilt/pwa/introduction-to-service-worker#registration_and_scope
+  registerSwPrefix: '/v4',
   workboxOpts: {
     swDest: 'static/service-worker.js',
     cleanupOutdatedCaches: true,
