231e4a091ee5a102d0f1d07e6f0ef75f85313254
Kevin Jennison <kevin.jennison1@gmail.com>
2020-01-31 13:50:56 -0800

    Create a Session object

--- src/utils/middleware/session.js
+++ src/utils/middleware/session.js
@@ -2,44 +2,56 @@ import { isNil } from 'lodash/lang'
 import { encodeBase64, decodeBase64 } from 'src/utils/encoding'
 import { withCookies } from 'src/utils/middleware/cookies'
 
+// Session adapted from:
+// https://github.com/expressjs/cookie-session/blob/master/index.js
+function Session(req) {
+  this.req = req
+  this.cookieName = 'tabWebSession'
+
+  this.serialize = val => encodeBase64(val)
+  this.deserialize = val => decodeBase64(val)
+
+  this.get = () => {
+    let decodedVal
+    try {
+      decodedVal = this.deserialize(this.req.cookie.get(this.cookieName))
+      return decodedVal
+    } catch (e) {
+      return undefined
+    }
+  }
+
+  this.set = val => {
+    let encodedVal
+    try {
+      // If the value is not defined, set the value to undefined
+      // so that the cookie will be deleted.
+      if (isNil(val)) {
+        encodedVal = undefined
+      } else {
+        encodedVal = this.serialize(val)
+      }
+    } catch (e) {
+      // TODO: log error
+      console.error(e) // eslint-disable-line no-console
+      return
+    }
+    this.req.cookie.set(this.cookieName, encodedVal)
+  }
+}
+
 const addSession = req => {
   if (!req.cookie) {
     throw new Error('The session middleware requires the cookies middleware.')
   }
-  const sessionCookieName = 'tabWebSession'
 
   try {
-    // Session getter/setter. Adapted from:
-    // https://github.com/expressjs/cookie-session/blob/master/index.js
+    const session = new Session(req)
     Object.defineProperty(req, 'session', {
       configurable: true,
       enumerable: true,
-      get: () => {
-        let decodedVal
-        try {
-          decodedVal = decodeBase64(req.cookie.get(sessionCookieName))
-          return decodedVal
-        } catch (e) {
-          return undefined
-        }
-      },
-      set: val => {
-        let encodedVal
-        try {
-          // If the value is not defined, set the value to undefined
-          // so that the cookie will be deleted.
-          if (isNil(val)) {
-            encodedVal = undefined
-          } else {
-            encodedVal = encodeBase64(val)
-          }
-        } catch (e) {
-          // TODO: log error
-          console.error(e) // eslint-disable-line no-console
-          return
-        }
-        req.cookie.set(sessionCookieName, encodedVal)
-      },
+      get: session.get,
+      set: session.set,
     })
   } catch (e) {
     console.error(e) // eslint-disable-line no-console
