f907358f5a56f418759092caf12065631bf1cdfd
Kevin Jennison <kevin.jennison1@gmail.com>
2020-03-19 17:53:32 -0700

    WIP - logout with user token

--- src/utils/auth/firebaseSessionHandler.js
+++ src/utils/auth/firebaseSessionHandler.js
@@ -6,12 +6,15 @@
 import fetch from 'isomorphic-unfetch'
 import { apiLogin, apiLogout } from 'src/utils/urls'
 
-export const setSession = async user => {
+export const setSession = async ({ shouldLogin, user }) => {
+  if (!user) {
+    throw new Error('The setSession function requires a valid "user" value.')
+  }
   const userToken = await user.getIdToken()
 
-  // If the user is authed, call login to set a cookie.
-  if (user) {
-    return fetch(apiLogin, {
+  // If the user is still authed, call login to set a cookie.
+  if (shouldLogin) {
+    await fetch(apiLogin, {
       method: 'POST',
       // eslint-disable-next-line no-undef
       headers: new Headers({
@@ -19,10 +22,11 @@ export const setSession = async user => {
       }),
       credentials: 'include',
     })
+    return
   }
 
-  // If the user is not authed, call logout to unset the cookie.
-  return fetch(apiLogout, {
+  // If the user is logging out, call logout to unset the cookie.
+  await fetch(apiLogout, {
     method: 'POST',
     // eslint-disable-next-line no-undef
     headers: new Headers({
--- src/utils/auth/hooks.js
+++ src/utils/auth/hooks.js
@@ -26,28 +26,38 @@ export const useFirebaseAuth = () => {
     }
   })
 
-  async function onChange(user) {
-    // Call server to update session. We need this to complete
-    // before setting the user state to ensure the session exists
-    // and thus server-side rendering will behave like client-side
-    // rendering.
-    try {
-      await setSession(user)
-    } catch (e) {
-      // TODO: log error
-      console.error(e) // eslint-disable-line no-console
+  // FIXME: we need the user's token when unsetting the session on logout,
+  //   and we only want to subscribe to firebase auth changes once.
+  useEffect(() => {
+    async function onChange(user) {
+      // When the user logs in or out to Firebase, call the server
+      // to update their session. We must wait for the session to be
+      // set before changing the user state because we want server-side
+      // rendering to match client-side user state.
+      try {
+        // If the Firebase user exists, the "shouldLogin" value is true,
+        // which means we'll call to set the session. The "user" value
+        // will either be the current user state or, if logging out, the
+        // user state immediately before logout. We need the Firebase user
+        // during logout so that we can send the user token to the logout
+        // endpoint to authorize logout. Using a token instead of relying
+        // on the session protects against CSRF attacks.
+        await setSession({ shouldLogin: !!user, user: user || state.user })
+      } catch (e) {
+        // TODO: log error
+        console.error(e) // eslint-disable-line no-console
+      }
+
+      setState({ initializing: false, user })
     }
 
-    setState({ initializing: false, user })
-  }
-
-  useEffect(() => {
     // Listen for auth state changes.
     const unsubscribe = firebase.auth().onAuthStateChanged(onChange)
 
     // Unsubscribe to the listener when unmounting.
     return () => unsubscribe()
-  }, [])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []) // FIXME
 
   return state
 }
