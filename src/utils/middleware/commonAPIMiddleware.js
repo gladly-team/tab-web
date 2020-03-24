2603886b158fd35b4cc873597ae8b9c6d7a12a12
Kevin Jennison <kevin.jennison1@gmail.com>
2020-03-23 17:41:49 -0700

    Only allow POST requests on API endpoints

--- src/utils/middleware/commonAPIMiddleware.js
+++ src/utils/middleware/commonAPIMiddleware.js
@@ -3,11 +3,14 @@ import session from 'src/utils/middleware/session'
 import addUserFromAuthorizationToken from 'src/utils/middleware/addUserFromAuthorizationToken'
 import authProtected from 'src/utils/middleware/authProtected'
 import customHeaderRequired from 'src/utils/middleware/customHeaderRequired'
+import onlyPostRequests from 'src/utils/middleware/onlyPostRequests'
 
 // Load environment variables.
 require('src/env')
 
 export default handler =>
-  customHeaderRequired(
-    cookies(session(addUserFromAuthorizationToken(authProtected(handler))))
+  onlyPostRequests(
+    customHeaderRequired(
+      cookies(session(addUserFromAuthorizationToken(authProtected(handler))))
+    )
   )
---
+++ src/utils/middleware/onlyPostRequests.js
@@ -0,0 +1,9 @@
+// Only allow POST requests.
+export default handler => async (req, res) => {
+  if (req.method !== 'POST') {
+    return res
+      .status(405)
+      .json({ error: `This request method is not allowed.` })
+  }
+  return handler(req, res)
+}
