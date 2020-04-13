40b8e702823205ecded525f6fd0500f84461500c
Kevin Jennison <kevin.jennison1@gmail.com>
2020-04-13 09:49:55 -0700

    Revert "Revert base path change: still use /v4* as primary but rewrite /newtab*"
    This reverts commit 3dc6120d8f9b1d3e2460273c910748791a748902.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -27,7 +27,7 @@ const App = props => {
     const isServiceWorkerEnabled = process.env.SERVICE_WORKER_ENABLED === 'true'
     if (isClientSide()) {
       if (isServiceWorkerEnabled) {
-        register('/v4/service-worker.js')
+        register('/newtab/service-worker.js')
         console.log('Registered the service worker.') // eslint-disable-line no-console
       } else {
         unregister()
