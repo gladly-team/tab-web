6010d83255d8ae7a24ffcc61451b83d18eae71e4
Kevin Jennison <kevin.jennison1@gmail.com>
2020-04-15 18:18:03 -0700

    Remove unused Jest config value

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,6 @@ module.exports = {
     '!**/node_modules/**',
   ],
   moduleNameMapper: {
-    // '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-    //   '<rootDir>/jest/fileMock.js',
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
   },
   setupFilesAfterEnv: ['<rootDir>/jest/setupTests.js'],
