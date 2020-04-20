a4306933993e45a7dfa754f91092e733d8bac6f0
Kevin Jennison <kevin.jennison1@gmail.com>
2020-04-20 11:14:11 -0700

    Add theme

---
+++ src/utils/theme.js
@@ -0,0 +1,23 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#9d4ba3',
+      contrastText: '#fff',
+    },
+    secondary: {
+      main: '#4a90e2',
+      contrastText: '#fff',
+    },
+  },
+  typography: {
+    fontSize: 14,
+    fontFamily: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+  },
+  shape: {
+    borderRadius: 2,
+  },
+})
+
+export default theme
