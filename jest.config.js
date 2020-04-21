800bbcf6e5cb207c740da8cf51f61130f8972f7c
Kevin Jennison <kevin.jennison1@gmail.com>
2020-04-21 10:53:25 -0700

    Remove FIXME

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,6 @@ module.exports = {
     '!**/node_modules/**',
     '!**/__generated__/**',
   ],
-  // FIXME: Codecov can't run directly in ZEIT Now. We
-  //   may want to set up a CI service.
   coverageDirectory: './coverage/',
   collectCoverage: true,
   moduleNameMapper: {
