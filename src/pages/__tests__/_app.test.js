20d17d4c7a37d658fd73caaea9799cfe2738c8e6
Kevin Jennison <kevin.jennison1@gmail.com>
2020-04-30 15:37:54 -0700

    Add tests

--- src/pages/__tests__/_app.test.js
+++ src/pages/__tests__/_app.test.js
@@ -8,6 +8,7 @@ import {
   // getAuthUserInfoFromDOM,
 } from 'src/utils/auth/user'
 import getMockNextJSContext from 'src/utils/testHelpers/getMockNextJSContext'
+import { withSession } from 'src/utils/middleware/session'
 
 jest.mock('next-offline/runtime')
 jest.mock('src/utils/auth/hooks')
@@ -39,6 +40,7 @@ beforeEach(() => {
   MockComponent.getInitialProps.mockReturnValue({
     some: 'data',
   })
+
   useFirebaseAuth.mockReturnValue({
     initializing: true,
     user: {
@@ -47,8 +49,19 @@ beforeEach(() => {
       emailVerified: true,
     },
   })
+
   isClientSide.mockReturnValue(true)
   isServerSide.mockReturnValue(false)
+
+  withSession.mockImplementation(req => {
+    req.cookies = ''
+    Object.defineProperty(req, 'session', {
+      configurable: true,
+      enumerable: true,
+      get: jest.fn(),
+      set: jest.fn(),
+    })
+  })
 })
 
 describe('_app.js', () => {
@@ -97,6 +110,69 @@ describe('_app.js: getInitialProps', () => {
     expect(initialProps.pageProps).toEqual({
       hi: 'there',
       datum: 3.1416,
+    })
+  })
+
+  it("returns an empty AuthUserInfo object if there isn't a session [server-side]", async () => {
+    expect.assertions(1)
+    isClientSide.mockReturnValue(false)
+    isServerSide.mockReturnValue(true)
+    withSession.mockImplementation(req => {
+      req.cookies = ''
+      Object.defineProperty(req, 'session', {
+        configurable: true,
+        enumerable: true,
+        get: jest.fn(() => undefined), // no session
+        set: jest.fn(),
+      })
+    })
+    const App = require('src/pages/_app.js').default
+    const initialProps = await App.getInitialProps({
+      Component: MockComponent,
+      ctx: getMockNextJSContext({ serverSide: true }),
+    })
+    expect(initialProps.AuthUserInfo).toEqual(createAuthUserInfo())
+  })
+
+  it('returns a populated AuthUserInfo object if there is a session [server-side]', async () => {
+    expect.assertions(1)
+    isClientSide.mockReturnValue(false)
+    isServerSide.mockReturnValue(true)
+    withSession.mockImplementation(req => {
+      req.cookies = ''
+      Object.defineProperty(req, 'session', {
+        configurable: true,
+        enumerable: true,
+        // Session exists
+        get: jest.fn(() => {
+          return {
+            AuthUserInfo: createAuthUserInfo({
+              AuthUser: {
+                id: 'user-id-from-session',
+                email: 'MrSessionGuy@example.com',
+                emailVerified: true,
+              },
+              token: 'some-token-here',
+              isClientInitialized: false,
+            }),
+          }
+        }),
+        set: jest.fn(),
+      })
+    })
+    const App = require('src/pages/_app.js').default
+    const initialProps = await App.getInitialProps({
+      Component: MockComponent,
+      ctx: getMockNextJSContext({ serverSide: true }),
+    })
+    expect(initialProps.AuthUserInfo).toEqual({
+      AuthUser: {
+        id: 'user-id-from-session',
+        email: 'MrSessionGuy@example.com',
+        emailVerified: true,
+      },
+      token: 'some-token-here',
+      isClientInitialized: false,
     })
   })
 })
