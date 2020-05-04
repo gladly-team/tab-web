873a38ea5f82fea2632a10b5bb6af2868fdf8c43
Kevin Jennison <kevin.jennison1@gmail.com>
2020-05-04 16:24:42 -0700

    Fix test

--- __mocks__/react-relay.js
+++ __mocks__/react-relay.js
@@ -1,5 +1,5 @@
 /* eslint-env jest */
 
 const mock = jest.genMockFromModule('react-relay')
-mock.createFragmentContainer = wrappedComponent => wrappedComponent
+mock.createFragmentContainer = jest.fn(wrappedComponent => wrappedComponent)
 module.exports = mock
