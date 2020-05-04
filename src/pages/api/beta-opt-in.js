eae19567a8d67028c72327e3a2e5479b6f6bc8ec
Kevin Jennison <kevin.jennison1@gmail.com>
2020-05-04 11:35:44 -0700

    Move pages into "containers" directory to allow non-page (test) files. See: https://github.com/zeit/next.js/issues/3728#issuecomment-363964953

--- src/pages/api/beta-opt-in.js
+++ src/pages/api/beta-opt-in.js
@@ -1,18 +1,3 @@
-import cookies from 'src/utils/middleware/cookies'
-import session from 'src/utils/middleware/session'
-import customHeaderRequired from 'src/utils/middleware/customHeaderRequired'
+import BetaOptInAPIEndpoint from 'src/containers/api/beta-opt-in'
 
-const optInCookieName = 'tabV4OptIn'
-
-// Sets or unsets a cookie that determines whether CloudFront
-// routes to this beta app or the exsting app.
-const handler = (req, res) => {
-  const isOptedIn = req.body.optIn === true
-  const cookieVal = isOptedIn ? 'enabled' : undefined
-  req.cookie.set(optInCookieName, cookieVal)
-  res.status(200).json({ status: true })
-}
-
-// Endpoint does not require the user to be authenticated. See:
-// https://github.com/gladly-team/tab-web#authentication-approach
-export default cookies(session(customHeaderRequired(handler)))
+export default BetaOptInAPIEndpoint
