a3120715b4fcde932ee3b79d2d366487893b41e9
Kevin Jennison <kevin.jennison1@gmail.com>
2020-05-06 10:58:31 -0700

    Add terms and privacy policy to Firebase login

--- src/components/FirebaseAuth.js
+++ src/components/FirebaseAuth.js
@@ -64,6 +64,10 @@ const FirebaseAuth = (props) => {
     // https://github.com/firebase/firebaseui-web#credential-helper
     // https://github.com/firebase/firebaseui-web/blob/bd710448caa34c4a47a2fd578d76be8506d392d8/javascript/widgets/config.js#L83
     credentialHelper: 'none',
+    // Terms of service URL
+    tosUrl: 'https://tab.gladly.io/terms/',
+    // Privacy policy URL
+    privacyPolicyUrl: 'https://tab.gladly.io/privacy/',
   }
 
   let signInMenu
