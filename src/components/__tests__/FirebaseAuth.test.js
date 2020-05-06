1564f9dfa1e06ae16528c919174189e2c8f8feec
Kevin Jennison <kevin.jennison1@gmail.com>
2020-05-06 14:00:57 -0700

    Add tests

--- src/components/__tests__/FirebaseAuth.test.js
+++ src/components/__tests__/FirebaseAuth.test.js
@@ -3,6 +3,7 @@ import { mount } from 'enzyme'
 import initFirebase from 'src/utils/auth/initFirebase'
 import { isClientSide } from 'src/utils/ssr'
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth'
+import { dashboardURL } from 'src/utils/urls'
 
 jest.mock('react-firebaseui/StyledFirebaseAuth')
 jest.mock('firebase/app')
@@ -51,6 +52,66 @@ describe('FirebaseAuth component', () => {
     const wrapper = mount(<FirebaseAuth {...mockProps} />)
     expect(wrapper.find(StyledFirebaseAuth).prop('uiConfig')).toEqual(
       expect.any(Object)
+    )
+  })
+
+  it('sets the FirebaseUI signInFlow to "popup"', () => {
+    expect.assertions(1)
+    const FirebaseAuth = require('src/components/FirebaseAuth').default
+    const mockProps = getMockProps()
+    const wrapper = mount(<FirebaseAuth {...mockProps} />)
+    const firebaseUIConfig = wrapper.find(StyledFirebaseAuth).prop('uiConfig')
+    expect(firebaseUIConfig.signInFlow).toEqual('popup')
+  })
+
+  it('sets the expected FirebaseUI signInOptions', () => {
+    expect.assertions(1)
+    const FirebaseAuth = require('src/components/FirebaseAuth').default
+    const mockProps = getMockProps()
+    const wrapper = mount(<FirebaseAuth {...mockProps} />)
+    const firebaseUIConfig = wrapper.find(StyledFirebaseAuth).prop('uiConfig')
+    expect(firebaseUIConfig.signInOptions).toEqual([
+      {
+        provider: 'google.com',
+        scopes: ['https://www.googleapis.com/auth/userinfo.email'],
+      },
+      {
+        provider: 'facebook.com',
+        scopes: ['email'],
+      },
+      {
+        provider: 'password',
+        requireDisplayName: false,
+      },
+    ])
+  })
+
+  it('sets the FirebaseUI signInSuccessUrl to the dashbaord URL', () => {
+    expect.assertions(1)
+    const FirebaseAuth = require('src/components/FirebaseAuth').default
+    const mockProps = getMockProps()
+    const wrapper = mount(<FirebaseAuth {...mockProps} />)
+    const firebaseUIConfig = wrapper.find(StyledFirebaseAuth).prop('uiConfig')
+    expect(firebaseUIConfig.signInSuccessUrl).toEqual(dashboardURL)
+  })
+
+  it('sets the correct FirebaseUI terms of service URL', () => {
+    expect.assertions(1)
+    const FirebaseAuth = require('src/components/FirebaseAuth').default
+    const mockProps = getMockProps()
+    const wrapper = mount(<FirebaseAuth {...mockProps} />)
+    const firebaseUIConfig = wrapper.find(StyledFirebaseAuth).prop('uiConfig')
+    expect(firebaseUIConfig.tosUrl).toEqual('https://tab.gladly.io/terms/')
+  })
+
+  it('sets the correct FirebaseUI privacy policy URL', () => {
+    expect.assertions(1)
+    const FirebaseAuth = require('src/components/FirebaseAuth').default
+    const mockProps = getMockProps()
+    const wrapper = mount(<FirebaseAuth {...mockProps} />)
+    const firebaseUIConfig = wrapper.find(StyledFirebaseAuth).prop('uiConfig')
+    expect(firebaseUIConfig.privacyPolicyUrl).toEqual(
+      'https://tab.gladly.io/privacy/'
     )
   })
 })
