5eed2135145ccb14a9f33706379dce0554faa959
Kevin Jennison <kevin.jennison1@gmail.com>
2020-05-07 11:13:55 -0700

    Reset modules after createRelayEnvironment tests

--- src/utils/__tests__/createRelayEnvironment.test.js
+++ src/utils/__tests__/createRelayEnvironment.test.js
@@ -1,19 +1,44 @@
-import { Network } from 'relay-runtime'
-// import { Environment, Network, RecordSource, Store } from 'relay-runtime'
-import initEnvironment from 'src/utils/createRelayEnvironment'
-
 jest.mock('relay-runtime')
 jest.mock('isomorphic-unfetch')
 jest.mock('src/utils/ssr')
 
+beforeEach(() => {
+  const { Environment, Network, Store } = require('relay-runtime')
+  Network.create.mockImplementation(() => ({
+    isMockRelayNetwork: true,
+  }))
+  Store.mockImplementation(() => ({
+    isMockRelayStore: true,
+  }))
+  Environment.mockImplementation(({ network, store }) => ({
+    isMockRelayEnvironment: true,
+    _network: network,
+    _store: store,
+  }))
+})
+
 afterEach(() => {
   jest.clearAllMocks()
+  jest.resetModules()
 })
 
 describe('createRelayEnvironment', () => {
+  it('returns a Relay environment', () => {
+    expect.assertions(1)
+    const initEnvironment = require('src/utils/createRelayEnvironment').default
+    const response = initEnvironment()
+    expect(response).toEqual({
+      isMockRelayEnvironment: true,
+      _network: { isMockRelayNetwork: true },
+      _store: { isMockRelayStore: true },
+    })
+  })
+
   it('calls Network.create', () => {
     expect.assertions(1)
+    const initEnvironment = require('src/utils/createRelayEnvironment').default
     initEnvironment()
-    expect(Network.create).toHaveBeenCalled()
+    const { Network } = require('relay-runtime')
+    expect(Network.create).toHaveBeenCalledWith(expect.any(Function))
   })
 })
