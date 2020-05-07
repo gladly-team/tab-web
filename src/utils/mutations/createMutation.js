5d11d8bb64341c5fe129da30df032b18430cfe35
Kevin Jennison <kevin.jennison1@gmail.com>
2020-05-07 11:52:40 -0700

    Use our own Promise mutation logic and remove dep

--- src/utils/mutations/createMutation.js
+++ src/utils/mutations/createMutation.js
@@ -1,10 +1,26 @@
-import commitMutation from 'relay-commit-mutation-promise'
+import { commitMutation as commitMutationDefault } from 'react-relay'
 import initEnvironment from 'src/utils/createRelayEnvironment'
 
 // TODO: throw if server-side
 // TODO: add tests
 
-// This is a mutation constructor.
+// Return a Promise when committing mutations.
+// https://github.com/facebook/relay/issues/1822#issuecomment-305906204
+const commitMutation = (environment, options) => {
+  return new Promise((resolve, reject) => {
+    commitMutationDefault(environment, {
+      ...options,
+      onError: (error) => {
+        reject(error)
+      },
+      onCompleted: (response) => {
+        resolve(response)
+      },
+    })
+  })
+}
+
+// https://relay.dev/docs/en/mutations#commitmutation
 const createMutation = (mutationConfig) => {
   const { mutation, variables } = mutationConfig
   const environment = initEnvironment()
