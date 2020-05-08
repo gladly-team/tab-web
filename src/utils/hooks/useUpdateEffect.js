3a9e32764184d17c3d880486293626f3d66fa262
Kevin Jennison <kevin.jennison1@gmail.com>
2020-05-07 17:35:51 -0700

    Add some helpful hooks

---
+++ src/utils/hooks/useUpdateEffect.js
@@ -0,0 +1,14 @@
+import { useEffect, useRef } from 'react'
+
+// Like useEffect but does not run on mount.
+// https://stackoverflow.com/a/53180013
+function useUpdateEffect(fn, inputs) {
+  const didMountRef = useRef(false)
+
+  useEffect(() => {
+    if (didMountRef.current) fn()
+    else didMountRef.current = true
+  }, inputs) // eslint-disable-line
+}
+
+export default useUpdateEffect
