20c116ac38397ee41607cd6a76ea8fcfbe14f394
Kevin Jennison <kevin.jennison1@gmail.com>
2020-05-15 17:32:22 -0700

    Add some placeholder card content and an achievements timeline

--- src/containers/achievements.js
+++ src/containers/achievements.js
@@ -2,15 +2,64 @@ import React from 'react'
 // import PropTypes from 'prop-types'
 // import { graphql } from 'react-relay'
 import { makeStyles } from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper'
+import grey from '@material-ui/core/colors/grey'
 import Typography from '@material-ui/core/Typography'
 import SettingsPage from 'src/components/SettingsPage'
 // import withAuthAndData from 'src/utils/pageWrappers/withAuthAndData'
+import ImpactGoal from 'src/components/ImpactGoal'
 
 const useStyles = makeStyles((theme) => ({
   contentContainer: {
     width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  timelineContainer: {
+    position: 'relative',
+    width: '100%',
+    marginTop: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    marginBottom: theme.spacing(0),
+    marginLeft: theme.spacing(2),
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  timelineBar: {
+    position: 'absolute',
+    zIndex: 10,
+    height: 'calc(100% - 100px)',
+    top: 40,
+    width: 24,
+    background: grey['300'],
+  },
+  timelineEnd: {
+    width: 70,
+    height: 70,
+    borderRadius: '50%',
+    background: grey['300'],
+    zIndex: 20,
+    marginTop: 40,
+  },
+  timelineEndTextContainer: {
+    marginTop: theme.spacing(2),
+    marginBottom: theme.spacing(4),
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  timelineEndText: {
+    lineHeight: '1.4em',
+  },
+  impactGoal: {
+    width: '100%',
+    maxWidth: 500,
+    minWidth: 275,
     margin: theme.spacing(2),
+    position: 'relative',
+    zIndex: 100,
   },
 }))
 
@@ -18,9 +67,35 @@ const Achievements = () => {
   const classes = useStyles()
   return (
     <SettingsPage>
-      <Paper elevation={1} className={classes.contentContainer}>
-        <Typography variant="body2">To-do!</Typography>
-      </Paper>
+      <div className={classes.contentContainer}>
+        <div className={classes.timelineContainer}>
+          <div className={classes.timelineBar} />
+          <ImpactGoal className={classes.impactGoal} />
+          <ImpactGoal className={classes.impactGoal} />
+          <ImpactGoal className={classes.impactGoal} />
+          <ImpactGoal className={classes.impactGoal} />
+          <ImpactGoal className={classes.impactGoal} />
+          <ImpactGoal className={classes.impactGoal} />
+          <ImpactGoal className={classes.impactGoal} />
+          <div className={classes.timelineEnd} />
+        </div>
+        <div className={classes.timelineEndTextContainer}>
+          <Typography
+            variant="overline"
+            align="center"
+            className={classes.timelineEndText}
+          >
+            Joined Tab for a Cause
+          </Typography>
+          <Typography
+            variant="overline"
+            align="center"
+            className={classes.timelineEndText}
+          >
+            January 1, 2020
+          </Typography>
+        </div>
+      </div>
     </SettingsPage>
   )
 }
