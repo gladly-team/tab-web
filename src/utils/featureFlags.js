adc6b6c6376c775c575bf5df864f1932348a8242
Kevin Jennison <kevin.jennison1@gmail.com>
2020-05-26 17:15:01 -0700

    Revert "Debug"
    This reverts commit e18088b4e3832a5e977f3577d9c0572a67879276.

--- src/utils/featureFlags.js
+++ src/utils/featureFlags.js
@@ -1,10 +1,7 @@
 // Important: add any new env variables to next.config.js.
 
-export const showDevelopmentOnlyDemoPages = () => {
-  // eslint-disable-next-line
-  console.log('process.env.FEATURE_FLAG_DEVELOPMENT_DEMO_PAGES', process.env.FEATURE_FLAG_DEVELOPMENT_DEMO_PAGES)
-  return process.env.FEATURE_FLAG_DEVELOPMENT_DEMO_PAGES === 'true'
-}
+export const showDevelopmentOnlyDemoPages = () =>
+  process.env.FEATURE_FLAG_DEVELOPMENT_DEMO_PAGES === 'true'
 
 export const showMockAchievements = () =>
   process.env.FEATURE_FLAG_MOCK_ACHIEVEMENTS === 'true'
