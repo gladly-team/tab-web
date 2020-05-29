9c0b670d3c6adb8127fef1abad576f51cc889cb4
Kevin Jennison <kevin.jennison1@gmail.com>
2020-05-29 12:55:06 -0700

    Add tests

--- src/containers/api/__tests__/beta-opt-in.test.js
+++ src/containers/api/__tests__/beta-opt-in.test.js
@@ -24,4 +24,38 @@ describe('API: beta-opt-in', () => {
       error: 'Invalid "optIn" value.',
     })
   })
+
+  it('returns a 200 response if the request body.optIn is set', async () => {
+    expect.assertions(2)
+    const mockReq = {
+      ...getMockReq(),
+      body: { optIn: false },
+    }
+    const mockRes = getMockRes()
+    await betaOptInAPI(mockReq, mockRes)
+    expect(mockRes.status).toHaveBeenCalledWith(200)
+    expect(mockRes.json).toHaveBeenCalledWith({ status: true })
+  })
+
+  it('sets the Tab v4 opt in cookie if body.optIn is true', async () => {
+    expect.assertions(1)
+    const mockReq = {
+      ...getMockReq(),
+      body: { optIn: true },
+    }
+    const mockRes = getMockRes()
+    await betaOptInAPI(mockReq, mockRes)
+    expect(mockReq.cookie.set).toHaveBeenCalledWith('tabV4OptIn', 'enabled')
+  })
+
+  it('unsets the Tab v4 opt in cookie if body.optIn is false', async () => {
+    expect.assertions(1)
+    const mockReq = {
+      ...getMockReq(),
+      body: { optIn: false },
+    }
+    const mockRes = getMockRes()
+    await betaOptInAPI(mockReq, mockRes)
+    expect(mockReq.cookie.set).toHaveBeenCalledWith('tabV4OptIn', undefined)
+  })
 })
