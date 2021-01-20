badf564540ffa74648fdd949d03e27eeb30a43f5
Kevin Jennison <kevin.jennison1@gmail.com>
2021-01-20 15:32:22 -0500

    Add tests

--- src/utils/hooks/__tests__/useData.test.js
+++ src/utils/hooks/__tests__/useData.test.js
@@ -9,7 +9,7 @@ jest.mock('react-relay')
 jest.mock('next-firebase-auth')
 jest.mock('src/utils/relayEnvironment')
 
-const getUnauthedAuthUser = () => ({
+const getUninitializedAuthUser = () => ({
   ...getMockAuthUser(),
   id: null,
   email: null,
@@ -43,7 +43,7 @@ describe('useData', () => {
     expect.assertions(1)
 
     // Defer any state-affecting logic.
-    useAuthUser.mockReturnValue(getUnauthedAuthUser())
+    useAuthUser.mockReturnValue(getUninitializedAuthUser())
     let relayQueryPromiseResolver
     const relayQueryPromise = new Promise((resolve) => {
       relayQueryPromiseResolver = resolve
@@ -83,7 +83,7 @@ describe('useData', () => {
     expect.assertions(1)
 
     // We will not initialize the AuthUser.
-    useAuthUser.mockReturnValue(getUnauthedAuthUser())
+    useAuthUser.mockReturnValue(getUninitializedAuthUser())
 
     let relayQueryPromiseResolver
     const relayQueryPromise = new Promise((resolve) => {
@@ -118,7 +118,7 @@ describe('useData', () => {
     expect.assertions(1)
 
     // We will not initialize the AuthUser.
-    useAuthUser.mockReturnValue(getUnauthedAuthUser())
+    useAuthUser.mockReturnValue(getUninitializedAuthUser())
 
     let relayQueryPromiseResolver
     const relayQueryPromise = new Promise((resolve) => {
@@ -143,5 +143,78 @@ describe('useData', () => {
       data: undefined,
       error: undefined,
     })
+  })
+
+  it('provides an authed AuthUser to `getRelayQuery`', async () => {
+    expect.assertions(1)
+
+    // Defer any state-affecting logic.
+    useAuthUser.mockReturnValue(getUninitializedAuthUser())
+    let relayQueryPromiseResolver
+    const relayQueryPromise = new Promise((resolve) => {
+      relayQueryPromiseResolver = resolve
+    })
+    const mockGetRelayQuery = jest.fn(async () => relayQueryPromise)
+
+    const { rerender, waitForNextUpdate } = renderHook(() =>
+      useData({ getRelayQuery: mockGetRelayQuery })
+    )
+
+    const mockAuthUser = getMockAuthUser()
+
+    // Update logic that affects the `useData` state.
+    await act(async () => {
+      useAuthUser.mockReturnValue(mockAuthUser)
+      relayQueryPromiseResolver({
+        query: `some query here`,
+        variables: {
+          some: 'thing',
+        },
+      })
+      rerender()
+
+      // https://github.com/testing-library/react-hooks-testing-library/issues/14#issuecomment-480225170
+      await waitForNextUpdate()
+    })
+    expect(mockGetRelayQuery).toHaveBeenCalledWith({ AuthUser: mockAuthUser })
+  })
+
+  it('provides an unauthed (but initialized) AuthUser to `getRelayQuery`', async () => {
+    expect.assertions(1)
+
+    // Defer any state-affecting logic.
+    useAuthUser.mockReturnValue(getUninitializedAuthUser())
+    let relayQueryPromiseResolver
+    const relayQueryPromise = new Promise((resolve) => {
+      relayQueryPromiseResolver = resolve
+    })
+    const mockGetRelayQuery = jest.fn(async () => relayQueryPromise)
+
+    const { rerender, waitForNextUpdate } = renderHook(() =>
+      useData({ getRelayQuery: mockGetRelayQuery })
+    )
+
+    const mockAuthUser = {
+      ...getMockAuthUser(),
+      id: null,
+      email: null,
+      clientInitialized: true,
+    }
+
+    // Update logic that affects the `useData` state.
+    await act(async () => {
+      useAuthUser.mockReturnValue(mockAuthUser)
+      relayQueryPromiseResolver({
+        query: `some query here`,
+        variables: {
+          some: 'thing',
+        },
+      })
+      rerender()
+
+      // https://github.com/testing-library/react-hooks-testing-library/issues/14#issuecomment-480225170
+      await waitForNextUpdate()
+    })
+    expect(mockGetRelayQuery).toHaveBeenCalledWith({ AuthUser: mockAuthUser })
   })
 })
