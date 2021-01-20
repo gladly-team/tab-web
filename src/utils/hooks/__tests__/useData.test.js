d56d0747b16213f537cb616359294d5cef977b24
Kevin Jennison <kevin.jennison1@gmail.com>
2021-01-20 15:25:41 -0500

    Add tests

--- src/utils/hooks/__tests__/useData.test.js
+++ src/utils/hooks/__tests__/useData.test.js
@@ -39,7 +39,7 @@ afterEach(() => {
 })
 
 describe('useData', () => {
-  it('returns fetched data', async () => {
+  it('returns fetched data when auth has initialized and a query is provided', async () => {
     expect.assertions(1)
 
     // Defer any state-affecting logic.
@@ -75,6 +75,72 @@ describe('useData', () => {
           username: 'MyUsername',
         },
       },
+      error: undefined,
+    })
+  })
+
+  it('returns undefined data when auth has not yet initialized', async () => {
+    expect.assertions(1)
+
+    // We will not initialize the AuthUser.
+    useAuthUser.mockReturnValue(getUnauthedAuthUser())
+
+    let relayQueryPromiseResolver
+    const relayQueryPromise = new Promise((resolve) => {
+      relayQueryPromiseResolver = resolve
+    })
+    const mockGetRelayQuery = async () => relayQueryPromise
+
+    const { result, rerender, waitForNextUpdate } = renderHook(() =>
+      useData({ getRelayQuery: mockGetRelayQuery })
+    )
+
+    // Update logic that affects the `useData` state.
+    await act(async () => {
+      relayQueryPromiseResolver({
+        query: `some query here`,
+        variables: {
+          some: 'thing',
+        },
+      })
+      rerender()
+
+      // https://github.com/testing-library/react-hooks-testing-library/issues/14#issuecomment-480225170
+      await waitForNextUpdate()
+    })
+    expect(result.current).toEqual({
+      data: undefined,
+      error: undefined,
+    })
+  })
+
+  it('returns undefined data when no query is provided', async () => {
+    expect.assertions(1)
+
+    // We will not initialize the AuthUser.
+    useAuthUser.mockReturnValue(getUnauthedAuthUser())
+
+    let relayQueryPromiseResolver
+    const relayQueryPromise = new Promise((resolve) => {
+      relayQueryPromiseResolver = resolve
+    })
+    const mockGetRelayQuery = async () => relayQueryPromise
+
+    const { result, rerender, waitForNextUpdate } = renderHook(() =>
+      useData({ getRelayQuery: mockGetRelayQuery })
+    )
+
+    // Update logic that affects the `useData` state.
+    await act(async () => {
+      useAuthUser.mockReturnValue(getMockAuthUser())
+      relayQueryPromiseResolver({}) // no query provideed
+      rerender()
+
+      // https://github.com/testing-library/react-hooks-testing-library/issues/14#issuecomment-480225170
+      await waitForNextUpdate()
+    })
+    expect(result.current).toEqual({
+      data: undefined,
       error: undefined,
     })
   })
