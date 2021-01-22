ceeeb67af038b3f96310e428c0626ad020f989b9
Kevin Jennison <kevin.jennison1@gmail.com>
2021-01-22 14:16:17 -0500

    Add test file

--- src/__tests__/pages/api/logout.test.js
+++ src/__tests__/pages/api/logout.test.js
@@ -2,10 +2,7 @@ import getMockReq from 'src/utils/testHelpers/mockReq'
 import getMockRes from 'src/utils/testHelpers/mockRes'
 
 jest.mock('next-firebase-auth')
-jest.mock('src/utils/middleware/addUserFromAuthorizationToken')
-jest.mock('src/utils/middleware/authProtected')
 jest.mock('src/utils/middleware/customHeaderRequired')
-jest.mock('src/utils/middleware/onlyPostRequests')
 jest.mock('src/utils/auth/initAuth')
 
 afterEach(() => {
@@ -13,13 +10,13 @@ afterEach(() => {
   jest.resetModules()
 })
 
-describe('API: login', () => {
+describe('API: logout', () => {
   it('returns a 200 response if there are no errors', async () => {
     expect.assertions(2)
-    const loginAPI = require('src/pages/api/login').default
+    const logout = require('src/pages/api/logout').default
     const mockReq = getMockReq()
     const mockRes = getMockRes()
-    await loginAPI(mockReq, mockRes)
+    await logout(mockReq, mockRes)
     expect(mockRes.status).toHaveBeenCalledWith(200)
     expect(mockRes.json).toHaveBeenCalledWith({
       success: true,
