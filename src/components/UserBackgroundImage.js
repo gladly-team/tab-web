5c2f8a1e0836dc9050c0145044d62de766ae8fbf
Alec Maliwanag <alecmaliwanag@alecs-mbp.lan>
2021-01-27 07:44:27 -0800

    load background image with styles

--- src/components/UserBackgroundImage.js
+++ src/components/UserBackgroundImage.js
@@ -1,17 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-// place holder component for the next PR
+import { makeStyles } from '@material-ui/core/styles'
+
 const UserBackgroundImage = ({ user }) => {
-  // temporary
-  console.log(user, 'props in userBackground')
-  return <div />
+  const useStyles = makeStyles(() => ({
+    background: {
+      boxShadow: 'rgba(0, 0, 0, 0.5) 0px 0px 120px inset',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundAttachment: 'fixed',
+      WebkitBackgroundSize: 'cover',
+      MozBackgroundSize: 'cover',
+      backgroundSize: 'cover',
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      right: 0,
+      left: 0,
+      zIndex: 'auto',
+      backgroundImage: `url(${user.backgroundImage.imageURL})`,
+    },
+  }))
+  const classes = useStyles()
+  return <div className={classes.background} />
 }
 export default UserBackgroundImage
 
 UserBackgroundImage.propTypes = {
   user: PropTypes.shape({
     backgroundImage: PropTypes.shape({
-      imageUrl: PropTypes.string,
+      imageURL: PropTypes.string,
       timestamp: PropTypes.string,
     }),
     id: PropTypes.string,
