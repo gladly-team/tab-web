4b7c66c19c24c78471f32b05ef397ede93a7f3e6
Alec Maliwanag <alecmaliwanag@alecs-mbp.lan>
2021-01-28 09:55:50 -0800

    pause work on css

--- src/components/UserBackgroundImage.js
+++ src/components/UserBackgroundImage.js
@@ -2,33 +2,51 @@ import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { makeStyles } from '@material-ui/core/styles'
 import { isNil } from 'lodash/lang'
-import moment from 'moment'
+// switch to dayjs
+import dayjs from 'dayjs'
+import isToday from 'dayjs/plugin/isToday'
+import { get } from 'lodash/object'
 import SetBackgroundDailyImageMutation from 'src/utils/mutations/SetBackgroundDailyImageMutation'
+import Fade from '@material-ui/core/Fade'
 
-const USER_BACKGROUND_OPTION_DAILY = 'daily'
-const UserBackgroundImage = ({ user }) => {
-  const { backgroundImage: imageURL } = user
-  const useStyles = makeStyles(() => ({
-    background: {
-      boxShadow: 'rgba(0, 0, 0, 0.5) 0px 0px 120px inset',
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center',
-      backgroundAttachment: 'fixed',
-      WebkitBackgroundSize: 'cover',
-      MozBackgroundSize: 'cover',
-      backgroundSize: 'cover',
-      position: 'absolute',
-      top: 0,
-      bottom: 0,
-      right: 0,
-      left: 0,
-      zIndex: 'auto',
-      backgroundImage: imageURL
+const FadeWrapper = ({ children }) => (
+  <Fade in out timeout={2000}>
+    {children}
+  </Fade>
+)
+dayjs.extend(isToday)
+const useStyles = makeStyles(() => ({
+  background: {
+    boxShadow: 'rgba(0, 0, 0, 0.5) 0px 0px 120px inset',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    backgroundAttachment: 'fixed',
+    backgroundSize: 'cover',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    zIndex: 'auto',
+    backgroundImage: (user) =>
+      user.backgroundImage.imageURL
         ? `url(${user.backgroundImage.imageURL})`
         : 'none',
-      backgroundColor: !imageURL ? '#4a90e2' : undefined,
-    },
-  }))
+    // backgroundColor: !imageURL ? '#4a90e2' : undefined,
+  },
+  tint: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    zIndex: 'auto',
+    // Needs to match shading in extension new tab page.
+    backgroundColor: `rgba(0, 0, 0, 0.15)`,
+  },
+}))
+const USER_BACKGROUND_OPTION_DAILY = 'daily'
+const UserBackgroundImage = ({ user }) => {
   const getNewDailyImageIfNeeded = ({
     backgroundImage: { timestamp },
     backgroundOption,
@@ -37,12 +55,8 @@ const UserBackgroundImage = ({ user }) => {
     if (backgroundOption !== USER_BACKGROUND_OPTION_DAILY || isNil(timestamp)) {
       return
     }
-    // Check if today is a different day (in local time) than
-    // the last time the background image changed.
-    const shouldChangeBackgroundImg = !moment()
-      .local()
-      .startOf('day')
-      .isSame(moment(timestamp).local().startOf('day'))
+    // by default dayjs displays in local time and compares in local
+    const shouldChangeBackgroundImg = !dayjs(timestamp).isToday()
     if (shouldChangeBackgroundImg) {
       SetBackgroundDailyImageMutation(id)
     }
@@ -50,8 +64,19 @@ const UserBackgroundImage = ({ user }) => {
   useEffect(() => {
     getNewDailyImageIfNeeded(user)
   }, [user])
-  const classes = useStyles()
-  return <div className={classes.background} />
+  //talk to kevin about this
+  const classes = useStyles(user)
+  return (
+    <div>
+      <FadeWrapper>
+        <div
+          className={classes.background}
+          Key={user.backgroundImage.imageURL}
+        />
+      </FadeWrapper>
+      <div data-test-id={'background-tint-overlay'} className={classes.tint} />
+    </div>
+  )
 }
 export default UserBackgroundImage
 
@@ -63,5 +88,15 @@ UserBackgroundImage.propTypes = {
     }),
     backgroundOption: PropTypes.string,
     id: PropTypes.string,
-  }).isRequired,
+  }),
+}
+UserBackgroundImage.defaultProps = {
+  user: {
+    backgroundImage: {
+      imageURL: '',
+      timestamp: '',
+    },
+    backgroundOption: 'daily',
+    id: '',
+  },
 }
