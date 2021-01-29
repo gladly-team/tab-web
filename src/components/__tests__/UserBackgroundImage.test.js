28be687c4db26b3b72121ef65b443b4a723534a4
Alec Maliwanag <alecmaliwanag@alecs-mbp.lan>
2021-01-28 22:26:26 -0800

    add additional test

--- src/components/__tests__/UserBackgroundImage.test.js
+++ src/components/__tests__/UserBackgroundImage.test.js
@@ -11,11 +11,28 @@ const getMockProps = () => ({
   },
 })
 
+beforeEach(() => {
+  jest.useFakeTimers()
+})
+
 describe('UserBackgroundImage component', () => {
   it('renders without error', () => {
     const UserBackgroundImage = require('src/components/UserBackgroundImage')
       .default
     const mockProps = getMockProps()
+    expect(() => {
+      mount(<UserBackgroundImage {...mockProps} />)
+    }).not.toThrow()
+  })
+  it('renders without error without background image info', () => {
+    const UserBackgroundImage = require('src/components/UserBackgroundImage')
+      .default
+    const mockProps = {
+      user: {
+        backgroundImage: {},
+        id: 'randomID',
+      },
+    }
     expect(() => {
       mount(<UserBackgroundImage {...mockProps} />)
     }).not.toThrow()
