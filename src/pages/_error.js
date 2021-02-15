62b5c981e1bb1284472e254b599592540c2eae1e
Kevin Jennison <kevin.jennison1@gmail.com>
2021-02-15 14:19:46 -0500

    Remove unneeded custom error logic (made obsolete by #102)

--- src/pages/_error.js
+++ src/pages/_error.js
@@ -1,73 +1,22 @@
-// Handles error logging to Sentry.
-// Adapted from:
-// https://github.com/vercel/next.js/blob/canary/examples/with-sentry/pages/_error.js
-
+// https://nextjs.org/docs/advanced-features/custom-error-page#customizing-the-error-page
 import React from 'react'
 import PropTypes from 'prop-types'
-import { getServerSideProps as NextErrGetSSP } from 'next/error'
-import * as Sentry from '@sentry/node'
 import ErrorPage from 'src/components/ErrorPage'
 
-const MyError = ({ hasGetServerSidePropsRun, err }) => {
-  if (!hasGetServerSidePropsRun && err) {
-    // `getServerSideProps`` is not called in case of
-    // https://github.com/vercel/next.js/issues/8592. As a workaround, we pass
-    // err via _app.js so it can be captured
-    Sentry.captureException(err)
-  }
+const MyError = () => <ErrorPage />
 
-  return <ErrorPage />
-}
-
-export const getServerSideProps = async ({ res, err, asPath }) => {
-  const errorProps = await NextErrGetSSP({
-    res,
-    err,
-  })
-
-  // Mark when getServerSideProps has run.
-  // Workaround for:
-  // https://github.com/vercel/next.js/issues/8592
-  errorProps.hasGetServerSidePropsRun = true
-
-  // Running on the server, the response object (`res`) is available.
-  //
-  // Next.js will pass an err on the server if a page's data fetching methods
-  // threw or returned a Promise that rejected
-  //
-  // Running on the client (browser), Next.js will provide an err if:
-  //
-  //  - a page's `getServerSideProps` threw or returned a Promise that rejected
-  //  - an exception was thrown somewhere in the React lifecycle (render,
-  //    componentDidMount, etc) that was caught by Next.js's React Error
-  //    Boundary. Read more about what types of exceptions are caught by Error
-  //    Boundaries: https://reactjs.org/docs/error-boundaries.html
-
-  if (res && res.statusCode === 404) {
-    // Opinionated: do not record an exception in Sentry for 404
-    return { statusCode: 404 }
-  }
-  if (err) {
-    Sentry.captureException(err)
-  } else {
-    // An empty error is unexpected and may indicate a bug in Next.js.
-    Sentry.captureException(
-      new Error(`_error.js getServerSideProps missing data at path: ${asPath}`)
-    )
-  }
-
-  return errorProps
+MyError.getInitialProps = ({ res, err }) => {
+  // eslint-disable-next-line no-nested-ternary
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
+  return { statusCode }
 }
 
 MyError.propTypes = {
-  // eslint-disable-next-line react/forbid-prop-types
+  // eslint-disable-next-line react/forbid-prop-types, react/no-unused-prop-types
   err: PropTypes.object,
-  hasGetServerSidePropsRun: PropTypes.bool,
 }
-
 MyError.defaultProps = {
   err: undefined,
-  hasGetServerSidePropsRun: false,
 }
 
 export default MyError
