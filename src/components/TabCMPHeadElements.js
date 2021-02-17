f81eb1f3f08a16ee6fa5d368bd8a3c65033637d1
Kevin Jennison <kevin.jennison1@gmail.com>
2021-02-17 16:12:31 -0500

    Add placeholder component

---
+++ src/components/TabCMPHeadElements.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+// Head scripts for tab-cmp.
+// https://github.com/gladly-team/tab-cmp
+// https://github.com/gladly-team/tab-cmp/blob/master/src/tagModified.html
+const TabCMPHeadElements = () => (
+  <>
+    <div>TODO</div>
+  </>
+)
+
+TabCMPHeadElements.displayName = 'TabCMPHeadElements'
+TabCMPHeadElements.propTypes = {}
+TabCMPHeadElements.defaultProps = {}
+
+export default TabCMPHeadElements
