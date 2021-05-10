25aafaa5d6171081b1ea8140ed8b2204392301cc
Alec Maliwanag <alecmaliwanag@alecs-mbp.lan>
2021-05-10 15:11:51 -0700

    will this work

--- .github/actions/set-qa-branch.js
+++ .github/actions/set-qa-branch.js
@@ -1,12 +1,14 @@
 const { Octokit } = require('@octokit/action')
-
-const octokit = new Octokit()
-const ref = await octokit.request(
-  'PATCH /repos/{owner}/{repo}/git/refs/{ref}',
-  {
-    owner: 'gladly-team',
-    repo: 'tab-web',
-    ref: 'refs/heads/qa',
-    sha: 'refs/heads/dev',
-  }
-)
+const run = async () => {
+  const octokit = new Octokit()
+  const ref = await octokit.request(
+    'PATCH /repos/{owner}/{repo}/git/refs/{ref}',
+    {
+      owner: 'gladly-team',
+      repo: 'tab-web',
+      ref: 'refs/heads/qa',
+      sha: 'refs/heads/dev',
+    }
+  )
+}
+run()
