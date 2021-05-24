ecca917d7d8e61478babe313035988e465a41c69
Kevin Jennison <kevin.jennison1@gmail.com>
2021-05-24 12:03:46 -0700

    Have Sentry ignore an expected error

--- src/utils/initSentry.js
+++ src/utils/initSentry.js
@@ -19,6 +19,11 @@ const initSentry = () => {
   alreadyInit = true
   Sentry.init({
     dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    ignoreErrors: [
+      // Expected error thrown in _app.js. See:
+      // https://github.com/zeit/next.js/issues/2476
+      /^routeChange aborted. This error can be safely ignored./,
+    ],
   })
 }
 
