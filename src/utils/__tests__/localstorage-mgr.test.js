d1a2ddb72bf6cf61d9aa6ac9f22e6ffa33aac959
Kevin Jennison <kevin.jennison1@gmail.com>
2021-08-02 15:01:26 -0400

    Clean up tests

--- src/utils/__tests__/localstorage-mgr.test.js
+++ src/utils/__tests__/localstorage-mgr.test.js
@@ -1,21 +1,13 @@
 /* global window */
 
 describe('local storage mng', () => {
-  it('contains expected methods', () => {
-    const localStorageMgrMethods = ['getItem', 'setItem', 'removeItem']
-    const localStorageMgr = require('src/utils/localstorage-mgr').default
-    localStorageMgrMethods.forEach((method) => {
-      expect(localStorageMgr[method]).not.toBeUndefined()
-    })
-  })
-
   it('getItem calls localStorage.getItem', () => {
     const localStorageMgr = require('src/utils/localstorage-mgr').default
     localStorageMgr.getItem('somekey')
     expect(window.localStorage.getItem).toHaveBeenCalledWith('somekey')
   })
 
-  it('getItem calls localStorage.setItem', () => {
+  it('setItem calls localStorage.setItem', () => {
     const localStorageMgr = require('src/utils/localstorage-mgr').default
     localStorageMgr.setItem('somekey', 'someval')
     expect(window.localStorage.setItem).toHaveBeenCalledWith(
@@ -24,10 +16,16 @@ describe('local storage mng', () => {
     )
   })
 
-  it('getItem calls localStorage.removeitemItem', () => {
+  it('removeItem calls localStorage.removeItem', () => {
     const localStorageMgr = require('src/utils/localstorage-mgr').default
     localStorageMgr.removeItem('somekey')
     expect(window.localStorage.removeItem).toHaveBeenCalledWith('somekey')
+  })
+
+  it('clear calls localStorage.clear', () => {
+    const localStorageMgr = require('src/utils/localstorage-mgr').default
+    localStorageMgr.clear()
+    expect(window.localStorage.clear).toHaveBeenCalledWith()
   })
 
   it("getItem doesn't throw even if localStorage throws", () => {
@@ -41,17 +39,33 @@ describe('local storage mng', () => {
     expect(localStorageMgr.getItem('somekey')).toBeNull()
   })
 
-  it("setItem doesn't throw", () => {
+  it("setItem doesn't throw even if localStorage throws", () => {
     const localStorageMgr = require('src/utils/localstorage-mgr').default
+    window.localStorage.setItem.mockImplementationOnce(() => {
+      throw new Error('This browser is not a fan of localStorage.')
+    })
     expect(() => {
       localStorageMgr.setItem('somekey', 'somevalue')
     }).not.toThrow()
   })
 
-  it("removeItem doesn't throw", () => {
+  it("removeItem doesn't throw even if localStorage throws", () => {
     const localStorageMgr = require('src/utils/localstorage-mgr').default
+    window.localStorage.removeItem.mockImplementationOnce(() => {
+      throw new Error('This browser is not a fan of localStorage.')
+    })
     expect(() => {
       localStorageMgr.removeItem('somekey')
+    }).not.toThrow()
+  })
+
+  it("clear doesn't throw even if localStorage throws", () => {
+    const localStorageMgr = require('src/utils/localstorage-mgr').default
+    window.localStorage.clear.mockImplementationOnce(() => {
+      throw new Error('This browser is not a fan of localStorage.')
+    })
+    expect(() => {
+      localStorageMgr.clear()
     }).not.toThrow()
   })
 })
