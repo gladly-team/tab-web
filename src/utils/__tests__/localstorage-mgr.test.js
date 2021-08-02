be09ce74fb3fa0be72fc56a2951ce8e8943bc10a
Kevin Jennison <kevin.jennison1@gmail.com>
2021-08-02 13:43:17 -0400

    Fix localStorage util

--- src/utils/__tests__/localstorage-mgr.test.js
+++ src/utils/__tests__/localstorage-mgr.test.js
@@ -1,3 +1,5 @@
+/* global window */
+
 describe('local storage mng', () => {
   it('contains expected methods', () => {
     const localStorageMgrMethods = ['getItem', 'setItem', 'removeItem']
@@ -7,11 +9,36 @@ describe('local storage mng', () => {
     })
   })
 
-  it("getItem doesn't throw", () => {
+  it('getItem calls localStorage.getItem', () => {
     const localStorageMgr = require('src/utils/localstorage-mgr').default
+    localStorageMgr.getItem('somekey')
+    expect(window.localStorage.getItem).toHaveBeenCalledWith('somekey')
+  })
+
+  it('getItem calls localStorage.setItem', () => {
+    const localStorageMgr = require('src/utils/localstorage-mgr').default
+    localStorageMgr.setItem('somekey', 'someval')
+    expect(window.localStorage.setItem).toHaveBeenCalledWith(
+      'somekey',
+      'someval'
+    )
+  })
+
+  it('getItem calls localStorage.removeitemItem', () => {
+    const localStorageMgr = require('src/utils/localstorage-mgr').default
+    localStorageMgr.removeItem('somekey')
+    expect(window.localStorage.removeItem).toHaveBeenCalledWith('somekey')
+  })
+
+  it("getItem doesn't throw even if localStorage throws", () => {
+    const localStorageMgr = require('src/utils/localstorage-mgr').default
+    window.localStorage.getItem.mockImplementationOnce(() => {
+      throw new Error('This browser is not a fan of localStorage.')
+    })
     expect(() => {
       localStorageMgr.getItem('somekey')
     }).not.toThrow()
+    expect(localStorageMgr.getItem('somekey')).toBeNull()
   })
 
   it("setItem doesn't throw", () => {
