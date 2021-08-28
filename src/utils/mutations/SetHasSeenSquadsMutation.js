051d1e78d66d675a1f76e472442773e73206a328
jedtan <jedtan@gmail.com>
2021-08-28 14:44:42 -0700

    TFAC-458 Adding SetHasSeenSquadsMutation

---
+++ src/utils/mutations/SetHasSeenSquadsMutation.js
@@ -0,0 +1,25 @@
+import { graphql } from 'react-relay'
+import callMutation from 'src/utils/mutations/callMutation'
+
+const mutation = graphql`
+  mutation SetHasSeenSquadsMutation($input: SetHasViewedIntroFlowInput!) {
+    setHasViewedIntroFlow(input: $input) {
+      user {
+        userId,
+        hasSeenSquads
+      }
+    }
+  }
+`
+
+const SetHasSeenSquadsMutation = ({ userId }) =>
+  callMutation({
+    mutation,
+    variables: {
+      input: {
+        userId,
+      },
+    },
+  })
+
+export default SetHasSeenSquadsMutation
