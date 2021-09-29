56589b4444af738e10ccb92cfc817e4953c06971
Alec Maliwanag <alecmaliwanag@alecs-mbp.lan>
2021-09-29 12:22:18 -0700

    try more things

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,8 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  webpackFinal: (config) => {
-    config.resolve.alias['@sentry/node'] = '@sentry/browser'
-    return config
+  webpackFinal: async (baseConfig) => {
+    const nextConfig = require('../next.config.js')
+    return { ...nextConfig.webpack, ...baseConfig }
   },
 }
