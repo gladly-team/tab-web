039230d40d80bdac0da55a5100e94b98b4bd3976
Kevin Jennison <kevin.jennison1@gmail.com>
2021-10-08 19:01:45 -0400

    Improve flushing promises for Jest 27

--- src/utils/testHelpers/flushAllPromises.js
+++ src/utils/testHelpers/flushAllPromises.js
@@ -1,14 +1,13 @@
+/* eslint-env jest */
+
 /**
  * Flush the Promise resolution queue. See:
  * https://github.com/facebook/jest/issues/2157
  * @return {Promise<undefined>}
  */
-const flushAllPromises = async () => {
-  await new Promise((resolve) =>
-    setTimeout(() => {
-      resolve()
-    }, 0)
-  )
-}
+// See latest:
+// https://github.com/facebook/jest/issues/2157#issuecomment-897935688
+const flushAllPromises = async () =>
+  new Promise(jest.requireActual('timers').setImmediate)
 
 export default flushAllPromises
