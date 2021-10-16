f99d849fcccf6d7906f0de31fa700195aa0f450d
Kevin Jennison <kevin.jennison1@gmail.com>
2021-10-16 13:18:31 -0400

    Create working markdown

--- src/components/Markdown.stories.jsx
+++ src/components/Markdown.stories.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import testInputA from 'src/utils/testHelpers/testInputA.md'
 import Markdown from './Markdown'
 
 export default {
@@ -11,15 +12,5 @@ const Template = (args) => <Markdown {...args} />
 
 export const demo = Template.bind({})
 demo.args = {
-  children: `# This is an H1
-    ## And here's an H2.
-    ### I'm an H3.
-    #### It's me, an H4!
-    ##### Hola, soy H5.
-    ###### Hey, H6 here.
-
-    Just a humble paragraph here, followed by...
-
-    ... another paragraph!
-  `,
+  children: testInputA,
 }
---
+++ src/utils/testHelpers/testInputA.md
@@ -0,0 +1,17 @@
+# This is an H1
+
+## And here's an H2.
+
+### I'm an H3.
+
+#### It's me, an H4!
+
+##### Hola, soy H5.
+
+###### Hey, H6 here.
+
+Just a humble paragraph here, followed by...
+
+... another paragraph!
+
+And a **bold**, then _italicized_, word with a [link here](https://example.com).
