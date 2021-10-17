5a318c436f2b26311a8a3fb4d1bcd599ab047082
Kevin Jennison <kevin.jennison1@gmail.com>
2021-10-17 10:33:15 -0400

    Use MUI components when rendering markdown

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -1,3 +1,5 @@
+// Pass props through to lots of wrapper components.
+/* eslint-disable react/jsx-props-no-spreading */
 import React, { useMemo } from 'react'
 import PropTypes from 'prop-types'
 import { unified } from 'unified'
@@ -5,6 +7,7 @@ import remarkParse from 'remark-parse'
 import remarkRehype from 'remark-rehype'
 import rehypeSanitize, { defaultSchema } from 'rehype-sanitize'
 import rehypeReact from 'rehype-react'
+import Typography from '@material-ui/core/Typography'
 
 const schema = defaultSchema
 
@@ -15,7 +18,22 @@ const processor = unified()
 
   // https://github.com/rehypejs/rehype-sanitize#use
   .use(rehypeSanitize, schema)
-  .use(rehypeReact, { createElement: React.createElement })
+  .use(rehypeReact, {
+    createElement: React.createElement,
+    components: {
+      h1: (props) => <Typography {...props} variant="h1" />,
+      h2: (props) => <Typography {...props} variant="h2" />,
+      h3: (props) => <Typography {...props} variant="h3" />,
+      h4: (props) => <Typography {...props} variant="h4" />,
+      h5: (props) => <Typography {...props} variant="h5" />,
+      h6: (props) => <Typography {...props} variant="h6" />,
+      p: (props) => <Typography {...props} variant="body2" paragraph />,
+
+      // Anchor will have content when rendered.
+      // eslint-disable-next-line jsx-a11y/anchor-has-content
+      a: (props) => <a target="_top" {...props} />,
+    },
+  })
 
 // TODO: map to MUI components
 
