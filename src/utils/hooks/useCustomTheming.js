58d8eab0ffdba7669ecf1cacf59b394e2f8bc59b
Kevin Jennison <kevin.jennison1@gmail.com>
2021-10-21 20:46:41 -0400

    Remove double comment // //

--- src/utils/hooks/useCustomTheming.js
+++ src/utils/hooks/useCustomTheming.js
@@ -1,7 +1,7 @@
 import { useContext, createContext } from 'react'
 
 export const CustomThemingContext = createContext(
-  () => {} // // `setCustomTheming` function. State managed in CustomThemeHOC.js.
+  () => {} // `setCustomTheming` function. State managed in CustomThemeHOC.js.
 )
 const useThemeContext = () => useContext(CustomThemingContext)
 export default useThemeContext
