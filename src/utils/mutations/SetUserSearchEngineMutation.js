53b0607a385678ff506c8e76e32e234ef8d07381
Jed Tan <jedtan@Jeds-MacBook-Pro.local>
2022-02-07 19:33:43 -0800

    Updating TODOs for new Mutations

--- src/utils/mutations/SetUserSearchEngineMutation.js
+++ src/utils/mutations/SetUserSearchEngineMutation.js
@@ -1,6 +1,7 @@
 import { graphql } from 'react-relay'
 import callMutation from 'src/utils/mutations/callMutation'
 
+// todo: @jedtan, edit returned field after completing TFAC-781
 const mutation = graphql`
   mutation SetUserSearchEngineMutation($input: SetUserSearchEngineInput!) {
     setUserSearchEngine(input: $input) {
