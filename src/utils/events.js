e6e023c33a2ea5b87ef2026f14960255fb6a85ed
Kevin Jennison <kevin.jennison1@gmail.com>
2022-02-14 12:18:19 -0500

    Add GA event call on signup

--- src/utils/events.js
+++ src/utils/events.js
@@ -1,10 +1,15 @@
 import fbq from 'src/utils/facebook-analytics'
 import rdt from 'src/utils/reddit-analytics'
+import gtag from 'src/utils/google-analytics'
 
 // Call once on a new user's first tab.
 export const accountCreated = () => {
   fbq('track', 'CompleteRegistration', { content_name: 'AccountCreated' })
   rdt('track', 'SignUp')
+
+  // TODO: for GA on pages, set custom user ID property
+  // https://developers.google.com/analytics/devguides/collection/ga4/reference/events#sign_up
+  gtag('event', 'sign_up')
 }
 
 // Call on each new tab.
