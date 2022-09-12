b831ccde47dc107caf569c4be4851dfc8361766c
Kevin Jennison <kevin.jennison1@gmail.com>
2022-09-12 17:57:27 -0400

    Lint the storybook directory

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,23 +1,24 @@
-const path = require('path');
-const dotenv = require('dotenv');
-const webpack = require('webpack');
-
+/* eslint import/no-extraneous-dependencies: 0 */
+const path = require('path')
+const dotenv = require('dotenv')
+const webpack = require('webpack')
 
 dotenv.config({
-	path: path.resolve(__dirname, '../.env.preview.info'),
-});
+  path: path.resolve(__dirname, '../.env.preview.info'),
+})
 
 // From:
 // https://github.com/storybookjs/storybook/issues/12270#issuecomment-1039631674
-const injectVars = Object.keys(process.env).reduce((c,key) => {
-  if(/^NEXT_PUBLIC_/.test(key)) {
-    c[`process.env.${key}`] = JSON.stringify(process.env[key]);
+const injectVars = Object.keys(process.env).reduce((c, key) => {
+  if (/^NEXT_PUBLIC_/.test(key)) {
+    // eslint-disable-next-line no-param-reassign
+    c[`process.env.${key}`] = JSON.stringify(process.env[key])
   }
-  return c;
+  return c
 }, {})
 
 function injectEnv(definitions) {
-  const env = 'process.env';
+  const env = 'process.env'
   if (!definitions[env]) {
     return {
       ...definitions,
@@ -25,27 +26,31 @@ function injectEnv(definitions) {
         Object.fromEntries(
           Object.entries(definitions)
             .filter(([key]) => key.startsWith(env))
-            .map(([key, value]) => [key.substring(env.length + 1), JSON.parse(value)]),
-        ),
+            .map(([key, value]) => [
+              key.substring(env.length + 1),
+              JSON.parse(value),
+            ])
+        )
       ),
-    };
+    }
   }
-  return definitions;
+  return definitions
 }
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   webpackFinal: (config) => {
-  	// Always use the browser version of the Sentry library.
-  	config.resolve.alias['@sentry/node'] = '@sentry/browser'
+    // Always use the browser version of the Sentry library.
+    // eslint-disable-next-line no-param-reassign
+    config.resolve.alias['@sentry/node'] = '@sentry/browser'
 
+    // eslint-disable-next-line no-param-reassign
     config.plugins = config.plugins.reduce((c, plugin) => {
-
-    	// Manually inject some environment variables. Otherwise,
-    	// only `STORYBOOK_*` prefix env vars are supported.
-    	// https://github.com/storybookjs/storybook/issues/12270#issuecomment-1039631674
-      if(plugin instanceof webpack.DefinePlugin) {
+      // Manually inject some environment variables. Otherwise,
+      // only `STORYBOOK_*` prefix env vars are supported.
+      // https://github.com/storybookjs/storybook/issues/12270#issuecomment-1039631674
+      if (plugin instanceof webpack.DefinePlugin) {
         return [
           ...c,
           new webpack.DefinePlugin(
@@ -57,12 +62,9 @@ module.exports = {
         ]
       }
 
-      return [
-        ...c,
-        plugin,
-      ]
-    }, []);
+      return [...c, plugin]
+    }, [])
 
-    return config;
+    return config
   },
 }
