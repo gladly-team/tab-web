c25a7eafc9d64de2671ee495d0be1cd30e64fa7d
Jedi Master <jedi@alkimiya.io>
2022-11-21 23:41:51 -0800

    Confetti Component

---
+++ src/components/Confetti.js
@@ -0,0 +1,73 @@
+import React, { useEffect, useRef } from 'react'
+import confetti from 'canvas-confetti'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(() => ({
+  canvas: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: '90000',
+    pointerEvents: 'none',
+  },
+}))
+const Celebration = () => {
+  const confettiCanvasRef = useRef(null)
+  const confettiFunc = () => {
+    const myConfetti = confetti.create(confettiCanvasRef.current, {
+      resize: true,
+    })
+    const count = 200
+    const defaults = {
+      origin: { y: 0.51, x: 0.9 },
+      angle: 140,
+    }
+
+    const fire = (particleRatio, opts) =>
+      myConfetti({
+        ...defaults,
+        ...opts,
+        particleCount: Math.floor(count * particleRatio),
+      })
+
+    fire(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    })
+    fire(0.2, {
+      spread: 60,
+    })
+    fire(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8,
+    })
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2,
+    })
+    fire(0.1, {
+      spread: 120,
+      startVelocity: 45,
+    })
+  }
+  const classes = useStyles()
+  useEffect(() => {
+    confettiFunc()
+  })
+  return (
+    <canvas
+      id="confettiCanvas"
+      width="400"
+      height="300"
+      className={classes.canvas}
+      ref={confettiCanvasRef}
+    />
+  )
+}
+
+Celebration.displayName = 'Celebration'
+
+export default Celebration
---
+++ src/components/__tests__/Confetti.test.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { shallow, mount } from 'enzyme'
+import confetti from 'canvas-confetti'
+
+jest.mock('canvas-confetti', () => ({
+  create: jest.fn().mockReturnValue(() => {}),
+}))
+
+beforeEach(async () => {
+  jest.clearAllMocks()
+})
+
+describe('Confetti component', () => {
+  it('renders without error', () => {
+    const Confetti = require('src/components/Confetti').default
+    expect(() => {
+      shallow(<Confetti />)
+    }).not.toThrow()
+  })
+
+  it('renders the confetti canvas', async () => {
+    const Confetti = require('src/components/Confetti').default
+    const wrapper = mount(<Confetti />)
+    expect(wrapper.find('canvas').length).toBe(1)
+  })
+
+  it('launches confetti', async () => {
+    const Confetti = require('src/components/Confetti').default
+    mount(<Confetti />)
+    expect(confetti.create).toHaveBeenCalledTimes(1)
+  })
+})
