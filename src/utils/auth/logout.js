2638f5df78bafcf5a40d0a0da9612ca5ba560cb9
Spicer Matthews <spicer@cloudmanic.com>
2024-11-12 22:40:53 -0800

    Cleared v4 cookie on logout.

--- src/utils/auth/logout.js
+++ src/utils/auth/logout.js
@@ -1,15 +1,21 @@
 import { clearAllServiceWorkerCaches } from 'src/utils/caching'
 import logger from 'src/utils/logger'
+import { deleteCookie } from 'cookies-next'
 import localStorageMgr from 'src/utils/localstorage-mgr'
 import { STORAGE_KEY_USERNAME } from 'src/utils/constants'
 
 const logout = async (AuthUser) => {
   try {
-    await AuthUser.signOut()
-
     // Clear the cache so it does not contain any authed content.
     await clearAllServiceWorkerCaches()
     await localStorageMgr.removeItem(STORAGE_KEY_USERNAME)
+
+    // Clear cookie for tabV4OptIn
+    await deleteCookie('tabV4OptIn')
+
+    // Must do this last, as it will cause the page to reload.
+    await AuthUser.signOut()
+
     return true
   } catch (e) {
     logger.error(e)
